4ce8b55c7a4c51f43eba6d9b94a806c88a3e3f11
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2020-09-09 11:42:20 +0200

    PhotosSearch renders a div with the right id

---
+++ test/PhotosSearch.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createContainer } from './helpers';
+import {
+  PhotosSearch,
+} from '../src/PhotosSearch';
+
+describe('PhotosSearch', () => {
+  let container, element, render;
+
+  beforeEach(() => {
+    ({
+      container,
+      element,
+      render
+    } = createContainer());
+  });
+
+  it('renders a div with the right id', () => {
+    render(<PhotosSearch />, container);
+
+    expect(element('div#photosSearch')).not.toBeNull();
+  });
+});
